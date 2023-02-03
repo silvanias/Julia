656d06e619b30f8661df98c5437e74fef2e10a19
Silas Maughan <silas.maughan@gmail.com>
2023-02-03 15:52:53 +0100

    Rudimentary signup check

--- juliaapp/routes.py
+++ juliaapp/routes.py
@@ -17,8 +17,17 @@ def login():
             return redirect(url_for('routes.gen'))
     return render_template('auth/login.html')
 
-@blueprint.route('/signup')
+@blueprint.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        if password1 != password2:
+            print("Passwords must match")
+        elif len(password1) < 5:
+            print('Passwords must be 5 or more characters')
     return render_template('auth/signup.html')
 
 @blueprint.route('/gen')
