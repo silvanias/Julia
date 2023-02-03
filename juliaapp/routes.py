33b9e6396ebac6bdc882d9210b556d75cfe01cf8
Silas Maughan <silas.maughan@gmail.com>
2023-02-03 15:47:17 +0100

    Rudimentary login check

--- juliaapp/routes.py
+++ juliaapp/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 
 blueprint = Blueprint('routes', __name__)
 
@@ -6,8 +6,15 @@ blueprint = Blueprint('routes', __name__)
 def landing():
     return render_template('landing.html')
 
-@blueprint.route('/login')
+@blueprint.route('/login', methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if email == "admin@admin" and password == "password":
+            print("logged in")
+            return redirect(url_for('routes.gen'))
     return render_template('auth/login.html')
 
 @blueprint.route('/signup')
